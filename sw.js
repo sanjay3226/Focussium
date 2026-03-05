f8d52d0036f80c041986de69231206a5ebcc61a5
sanjay3226 <meowstudy9999@gmail.com>
2026-03-05 14:23:11 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'focussium-v1';
+const ASSETS_TO_CACHE = [
+    './index.html',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request).catch(() => {
+                // Optional: return a fallback page if network fails and not in cache
+            });
+        })
+    );
+});
